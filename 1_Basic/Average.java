66438dbab2a30aefe15686919d0cf5437e4c90e8
01themanish <manishkr9341@gmail.com>
2026-02-27 19:58:18 +0530

    Improved formatting in program

--- 1_Basic/Average.java
+++ 1_Basic/Average.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        Average obj=new Average();
-        System.out.print("Enter any three number: ");
-        int x=sc.nextInt();
-        int y=sc.nextInt();
-        int z=sc.nextInt();
-        System.out.println("Average of "+x+", "+y+" and "+z+" is: "+obj.AverageofNumbers(x, y, z));
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            Average obj=new Average();
+            System.out.print("Enter any three number: ");
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            System.out.println("Average of "+x+", "+y+" and "+z+" is: "+obj.AverageofNumbers(x, y, z));
+            sc.close();
+        }
     }
     public double  AverageofNumbers(int x, int y, int z){
         int total=x+y+z;
--- 1_Basic/sumOfTwoNo.java
+++ 1_Basic/sumOfTwoNo.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class sumOfTwoNo{
     public static void main(){
-        Scanner sc=new Scanner(System.in);
-        sumOfTwoNo obj=new sumOfTwoNo();
-        System.out.print("Enter any two number: ");
-        int x=sc.nextInt();
-        int y=sc.nextInt();
-        System.out.println("Sum of "+x+" + "+y+" = "+obj.sum(x, y));
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            sumOfTwoNo obj=new sumOfTwoNo();
+            System.out.print("Enter any two number: ");
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            System.out.println("Sum of "+x+" + "+y+" = "+obj.sum(x, y));
+            sc.close();
+        }
     }
     public int sum(int x, int y){
         return x+y;
