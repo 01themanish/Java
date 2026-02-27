b23a2e9d70be3f9b4dbdddfc44d2a107a867ca18
01themanish <manishkr9341@gmail.com>
2026-02-27 19:32:47 +0530

    Added Basic Program 1 - Sum of Two Numbers

---
+++ 1. Basic/Average.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Average obj=new Average();
+        System.out.print("Enter any three number: ");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        System.out.println("Average of "+x+", "+y+" and "+z+" is: "+obj.AverageofNumbers(x, y, z));
+    }
+    public double  AverageofNumbers(int x, int y, int z){
+        int total=x+y+z;
+        double avg=total/3.0;
+        return avg;
+    }
+}
