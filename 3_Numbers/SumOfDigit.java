8e7b8e165322269c69b16549241ba0d934088268
01themanish <manishkr9341@gmail.com>
2026-03-09 14:47:19 +0530

    Added Numbers Program 20 - find the Sum of digits of a number

---
+++ 3_Numbers/SumOfDigit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main() {
+        try (Scanner sc=new Scanner(System.in)){
+            System.out.print("Enter a number: ");
+            int num=sc.nextInt();
+            int sum=0;
+            while(num!=0){
+                sum+=num%10;
+                num=num/10;
+            }
+            System.out.println("The sum of total digit is: "+sum);
+        }
+    }
+}
