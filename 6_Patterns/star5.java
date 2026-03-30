adef1046753d06ee2e0a0736ec0ad9ffc1c1956d
01themanish <manishkr9341@gmail.com>
2026-03-31 00:40:44 +0530

    Adding Program 35 - Star Patter5

--- 6_Patterns/star5.java
+++ 6_Patterns/star5.java
@@ -2,26 +2,28 @@ import java.util.Scanner;
 
 public class star5 {
     public static void main() {
-        Scanner sc = new Scanner(System.in);
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter number of rows: ");
+            int n = sc.nextInt();
 
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+            for(int i = 1; i <= 2*n - 1; i++) {
 
-        for(int i = 1; i <= 2*n - 1; i++) {
+                int row = (i <= n) ? i : 2*n - i;
 
-            int row = (i <= n) ? i : 2*n - i;
-
-            for(int j = 1; j <= n + row - 1; j++) {
-                if(j <= n - row) {
-                    System.out.print(" ");
-                } else {
-                    System.out.print("*");
+                for(int j = 1; j <= n + row - 1; j++) {
+                    if(j <= n - row) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
                 }
+
+                System.out.println();
             }
 
-            System.out.println();
+            sc.close();
         }
 
-        sc.close();
+        
     }
 }
