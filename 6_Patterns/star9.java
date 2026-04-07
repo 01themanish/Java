89adfe1fd88f2fdf90dbf33a43576dd8734a97de
01themanish <manishkr9341@gmail.com>
2026-04-08 00:14:20 +0530

    Adding Program 39 - Star Pattern9

---
+++ 6_Patterns/star9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class star9 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter number of rows: ");
+            int num = sc.nextInt();
+
+            for(int i=1; i<=num; i++){
+                for(int j=1; j<=i; j++){
+                    if(j==1 || j==i || i==num)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
